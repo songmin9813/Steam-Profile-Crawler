228583937f39faedf59e5caeabd82b99a693d9d7
songmin9813 <mithan1@naver.com>
2020-10-08 14:37:51 +0900

    still in progress

--- test.py
+++ test.py
@@ -1,6 +1,52 @@
+import re
 import requests
-from bs4 import BeautifulSoup
+import json
+import time
 
-webpage = requests.get("https://steamcommunity.com/id/songmin9813/games/?tab=all")
-soup=BeautifulSoup(webpage.content, "html.parser")
-print(soup.find_all(attrs={'class':''}))
+
+def print_steam_crawl(steamID,ID_Range,fileNum):
+    userCount=int(fileNum)
+    for n in range(ID_Range):
+        #time.sleep(20)
+        steamID=steamID+1
+        print('current data:', steamID ,'\n')
+        string_ID=str(steamID)
+        url='https://steamcommunity.com/profiles/'+string_ID+'/games/?tab=all&sort=playtime'
+        try: #private user check
+            html=requests.get(url).text
+            if 'profile_fatalerror' in html:
+                steamID=steamID-1
+                print("427 Error")
+                #time.sleep(1800)
+                continue
+            htmlSearch=re.search(r'var rgGames =(.+?);',html,re.S)
+            gameList=htmlSearch.group(1)
+            SteamGame=json.loads(gameList)
+        except:
+            continue
+        if(str(SteamGame)!="[]" and 'hours_forever' in gameList):
+            userCount=userCount+1
+            f=open('C:/Users/mitha/OneDrive/바탕 화면/dev/Crawling_Data/'+string_ID+'.csv','w')
+            f.write('GameName,Id:'+string_ID+'\n')
+            for course in SteamGame:
+                try: #empty playtime check
+                    gameName='{name}'.format(**course)
+                    gameName=gameName.replace(',',' ')#if comma inside gamename, replace space
+                    gameTime='{hours_forever}'.format(**course)
+                    gameTime=gameTime.replace(',',' ')
+                    if(float(gameTime)>=3.0):
+                        f.write(gameName+','+gameTime+'\n')
+                except:
+                    continue
+            f.close()
+        else:
+            continue
+
+
+pages=1
+#now=datetime.now()
+print_steam_crawl(76561198048511277,pages,0)
+#https://steamcommunity.com/profiles/76561198120029537/games/?tab=all&sort=playtime->test 427 error
+#76561197960265728 first steam user profiles
+
+
