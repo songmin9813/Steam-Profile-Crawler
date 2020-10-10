f7448e799b6d8e42b8598aef888002f83f822c52
songmin9813 <mithan1@naver.com>
2020-10-10 15:44:41 +0900

    add how many games, badges, etc

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import re
 import requests
 import json
+from bs4 import BeautifulSoup
 import time
 
 
 def print_steam_crawl(steamID,ID_Range,fileNum):
     for n in range(ID_Range):
-        userCount = 0
         #time.sleep(20)
         steamID=steamID+1
         print('current data:', steamID ,'\n')
@@ -19,16 +19,12 @@ def print_steam_crawl(steamID,ID_Range,fileNum):
                 print("427 Error")
                 #time.sleep(1800)
                 continue
-            htmlSearch=re.search(r'var rgGames =(.+?);',html,re.S)
-            print(htmlSearch)
+            htmlSearch=re.search('var rgGames =(.+?);',html,re.S)
             gameList=htmlSearch.group(1)
-            #print(gameList)
             SteamGame=json.loads(gameList)
-            #print(SteamGame)
         except:
             continue
         if(str(SteamGame)!="[]" and 'hours_forever' in gameList):
-            userCount=userCount+1
             f=open('C:/Users/mitha/OneDrive/바탕 화면/dev/Crawling_Data/'+string_ID+'.csv','w')
             f.write('GameName,Id:'+string_ID+'\n')
             for course in SteamGame:
@@ -39,19 +35,14 @@ def print_steam_crawl(steamID,ID_Range,fileNum):
                     gameTime=gameTime.replace(',',' ')
                     #if(float(gameTime)>=3.0):
                     f.write(gameName+','+gameTime+'\n')
-                    userCount = userCount + 1
                 except:
-                    userCount = userCount + 1
                     continue
-            f.write('\n'+'Counted Games'+','+str(userCount)+'\n')
         else:
             continue
         html = requests.get('https://steamcommunity.com/profiles/'+string_ID+'/games/?tab=recent').text
-        htmlSearch = re.search(r'var rgGames =(.+?);', html, re.S)
-        print(htmlSearch)
+        htmlSearch = re.search('var rgGames =(.+?);', html, re.S)
         gameList = htmlSearch.group(1)
         SteamGame = json.loads(gameList)
-        print(SteamGame)
         if (str(SteamGame) != "[]" and 'hours' in gameList):
             f.write('\n'+'Recently Played'+'\n')
             for course in SteamGame:
@@ -64,12 +55,30 @@ def print_steam_crawl(steamID,ID_Range,fileNum):
                     f.write(gameName+','+gameTime+'\n')
                 except:
                     continue
+        f.write('\n')
+        html = requests.get('https://steamcommunity.com/profiles/' + string_ID).text
+        soup=BeautifulSoup(html,"html.parser")
+        total=soup.select("span.profile_count_link_total")
+        namevalue=soup.select("span.count_link_label")
+        for key in namevalue:
+            try:
+                sentencevalue = key.text.join(key.text.split())
+                f.write(sentencevalue+',')
+            except:
+                continue
+        f.write('\n')
+        for key in total:
+            try:
+                sentence = key.text.join(key.text.split())
+                if(sentence==''):
+                    sentence='0'
+                f.write(sentence+',')
+            except:
+                continue
         f.close()
-        #f.write(str(userCount)+'\n')
 
 
 pages=1
-#now=datetime.now()
 print_steam_crawl(76561198048511277,pages,0)
 #https://steamcommunity.com/profiles/76561198120029537/games/?tab=all&sort=playtime->test 427 error
 #76561197960265728 first steam user profiles
