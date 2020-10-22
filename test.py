74231ff3e4e65fbe64776da0032189a843a8cbc0
songmin9813 <mithan1@naver.com>
2020-10-22 23:28:58 +0900

    finally add dynamic info!

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ from selenium.webdriver.support import expected_conditions as EC
 def print_steam_crawl(steamID, ID_Range, fileNum):
     for n in range(ID_Range):
         # time.sleep(20)
-        latedgame = 0
         steamID = steamID + 1
         print('current data:', steamID, '\n')
         string_ID = str(steamID)
@@ -37,7 +36,6 @@ def print_steam_crawl(steamID, ID_Range, fileNum):
                     gameName = gameName.replace(',', ' ')  # if comma inside gamename, replace space
                     gameTime = '{hours_forever}'.format(**course)
                     gameTime = gameTime.replace(',', '')
-
                     # gameData='{logo}'.format(**course)
                     # gameData=gameData[50:-19]
                     # html=requests.get('https://store.steampowered.com/app/'+gameData).text
@@ -87,18 +85,19 @@ def print_steam_crawl(steamID, ID_Range, fileNum):
             except:
                 continue
         options=webdriver.ChromeOptions()
-        #options.add_argument('headless')
-        #options.add_argument('disable-gpu')
-        #driver=webdriver.Chrome('chromedriver',options=options)
-        #driver.get('https://store.steampowered.com/wishlist/profiles/' + string_ID)
-        url='https://store.steampowered.com/wishlist/profiles/' + string_ID
-        wishlist_url = json.loads(re.findall(r'g_strWishlistBaseURL = (".*?");', requests.get(url).text)[0])
-        data = requests.get(wishlist_url + 'wishlistdata/?p=0').json()
-        print(json.dumps(data, indent=4))
+        options.add_argument('headless')
+        options.add_argument('disable-gpu')
+        driver=webdriver.Chrome('chromedriver',options=options)
+        driver.get('https://www.steamwishlistcalculator.com/?id=' + string_ID+'&currency=US')
+        element=WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,'titleCount')))
+        f.write(element.text)
+        #soup = BeautifulSoup(html, "html.parser")
+        #wishlist = soup.find({'id':'titleCount'})
+        #print(wishlist)
         f.close()
 
 
 pages = 1
-print_steam_crawl(76561198048511277, pages, 0)
+print_steam_crawl(76561198261943700, pages, 0)
 # https://steamcommunity.com/profiles/76561198120029537/games/?tab=all&sort=playtime->test 427 error
 # 76561197960265728 first steam user profiles, 76561198261943701 showmkk LOL, 76561198048511278 ME
